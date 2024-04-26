8fecba69e29a311e417e108fedb466b131365543
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-04-26 06:27:20 -0300

    First Commit

---
+++ src/main/java/com/barataribeiro/sabia/SabiaApplication.java
@@ -0,0 +1,13 @@
+package com.barataribeiro.sabia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barataribeiro/sabia/SabiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barataribeiro.sabia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SabiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
