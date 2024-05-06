28262a855e2384a674850aa2770b3b4a161bbc22
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-05-06 09:11:52 -0300

    feat: implement toggleVerifyUser route for admins

---
+++ server/src/main/java/com/barataribeiro/sabia/service/AdminService.java
@@ -0,0 +1,45 @@
+package com.barataribeiro.sabia.service;
+
+import com.barataribeiro.sabia.dto.admin.AdminEditProfileRequestDTO;
+import com.barataribeiro.sabia.dto.user.PublicProfileResponseDTO;
+import com.barataribeiro.sabia.exceptions.others.BadRequest;
+import com.barataribeiro.sabia.exceptions.user.UserNotFound;
+import com.barataribeiro.sabia.model.Roles;
+import com.barataribeiro.sabia.model.User;
+import com.barataribeiro.sabia.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AdminService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public PublicProfileResponseDTO updateUserProfile(String userId, AdminEditProfileRequestDTO body, String principalName) {
+        return null;
+    }
+
+    public Boolean toggleVerifyUser(String userId, String principalName) {
+        User user = userRepository.findById(userId).orElseThrow(UserNotFound::new);
+
+        if (user.getUsername().equals(principalName)) throw new BadRequest("Admins can't verify themselves.");
+        if (user.getRole().equals(Roles.ADMIN))
+            throw new BadRequest("Admins can't verify other admins. They are already verified.");
+
+        user.setIs_verified(!user.getIs_verified());
+
+        userRepository.save(user);
+
+        return user.getIs_verified();
+    }
+
+    public Boolean banUser(String userId, String principalName) {
+        return true;
+    }
+
+    @Transactional
+    public void deleteUser(String userId, String principalName) {
+    }
+}
