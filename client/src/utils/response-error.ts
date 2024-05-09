57a77cdbce6b5af2720eaa2b49b08ab2b0a40be8
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-05-09 11:00:31 -0300

    feat: add starting utils functions

---
+++ client/src/utils/response-error.ts
@@ -0,0 +1,24 @@
+import { State } from "@/interfaces/actions"
+
+const DEFAULT_MESSAGE_EN = "An error occurred. Please try again."
+const DEFAULT_MESSAGE_BR = "Ocorreu um erro. Por favor, tente novamente."
+
+export default function ApiError(error: unknown, locale: string): State {
+    console.error(error)
+
+    const state: State = {
+        ok: false,
+        client_error: null,
+        response: null
+    }
+
+    return {
+        ...state,
+        client_error:
+            error instanceof Error
+                ? error.message
+                : locale === "en"
+                  ? DEFAULT_MESSAGE_EN
+                  : DEFAULT_MESSAGE_BR
+    }
+}
