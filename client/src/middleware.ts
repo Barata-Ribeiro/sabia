a7b5ab6efe602aed7ce51d07c63e3522e068ca2b
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-05-11 08:34:52 -0300

    refactor: add auth to middleware

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,14 +1,52 @@
-import createMiddleware from "next-intl/middleware"
+import verifyToken from "@/utils/validate-token"
+import createIntlMiddleware from "next-intl/middleware"
+import { NextRequest, NextResponse } from "next/server"
 
-export default createMiddleware({
-    // A list of all locales that are supported
+const EXCLUDE_PATHS = [
+    "/",
+    "/auth/register",
+    "/auth/forgot-password",
+    "/auth/reset-password",
+    "/terms-of-use",
+    "/privacy-policy"
+]
+
+const INTL_CONFIG = {
     locales: ["en", "pt-BR"],
-
-    // Used when no locale matches
     defaultLocale: "en"
-})
+}
+
+export async function middleware(req: NextRequest) {
+    const auth_token = req.cookies.get("auth_token")?.value
+    const path = req.nextUrl.pathname
+    let isAuthenticated = false
+
+    const inlMiddleware = createIntlMiddleware({
+        locales: INTL_CONFIG.locales,
+        defaultLocale: INTL_CONFIG.defaultLocale,
+        localePrefix: "always"
+    })
+
+    const response = inlMiddleware(req)
+
+    if (auth_token) {
+        const isAuthTokenValid = await verifyToken(auth_token)
+        if (isAuthTokenValid) isAuthenticated = true
+    }
+
+    if (
+        !isAuthenticated &&
+        !EXCLUDE_PATHS.some((path) => req.nextUrl.pathname.includes(path))
+    ) {
+        const url = req.nextUrl.clone()
+        url.pathname = INTL_CONFIG.defaultLocale + "/"
+        return NextResponse.redirect(url)
+    }
+
+    return response
+}
 
 export const config = {
     // Match only internationalized pathnames
-    matcher: ["/", "/(pt-BR|en)/:path*"]
+    matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"]
 }
