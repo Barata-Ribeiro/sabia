86d2639ac7f7f92db08549afc9f507d2a124513b
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-05-14 15:52:09 -0300

    refactor: remove messy auth, TODO: rewrite auth logic

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,49 +1,40 @@
 import verifyToken from "@/utils/validate-token"
 import createIntlMiddleware from "next-intl/middleware"
-import { NextRequest, NextResponse } from "next/server"
+import { NextRequest } from "next/server"
 
-const EXCLUDE_PATHS = [
-    "/",
-    "/auth/register",
-    "/auth/password-lost",
-    "/auth/password-reset",
-    "/terms-of-use",
-    "/privacy-policy"
-]
-
-const INTL_CONFIG = {
+const inlMiddleware = createIntlMiddleware({
     locales: ["en", "pt-BR"],
-    defaultLocale: "en"
-}
+    defaultLocale: "en",
+    localePrefix: "always"
+})
 
 export async function middleware(req: NextRequest) {
-    const authToken = req.cookies.get("auth_token")?.value
-    const reqPath = req.nextUrl.pathname
-    let isAuthenticated = false
-
-    const inlMiddleware = createIntlMiddleware({
-        locales: INTL_CONFIG.locales,
-        defaultLocale: INTL_CONFIG.defaultLocale,
-        localePrefix: "always"
-    })
-
     const response = inlMiddleware(req)
+    const authToken = req.cookies.get("auth_token")?.value
+    let isAuthenticated = false
 
     if (authToken) {
         const isAuthTokenValid = await verifyToken(authToken)
         if (isAuthTokenValid) isAuthenticated = true
     }
 
-    if (!isAuthenticated && !EXCLUDE_PATHS.some((path) => reqPath.includes(path))) {
-        const url = req.nextUrl.clone()
-        url.pathname = INTL_CONFIG.defaultLocale + "/"
-        return NextResponse.redirect(url)
-    }
+    // TODO... AUTH REDIRECT LOGIC
 
     return response
 }
 
 export const config = {
     // Match only internationalized pathnames
-    matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"]
+    matcher: [
+        // Enable a redirect to a matching locale at the root
+        "/",
+
+        // Set a cookie to remember the previous locale for
+        // all requests that have a locale prefix
+        "/(en|pt-BR)/:path*",
+
+        // Enable redirects that add missing locales
+        // (e.g. `/pathnames` -> `/en/pathnames`)
+        "/((?!api|_next|_vercel|.*\\..*).*)"
+    ]
 }
