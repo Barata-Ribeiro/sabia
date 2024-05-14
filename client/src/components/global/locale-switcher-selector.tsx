f7d255fa18ed5e8a095d51eaaa6606ba7b27d35a
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-05-14 15:36:49 -0300

    refactor: adjust locale switcher flow

---
+++ client/src/components/global/locale-switcher-selector.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { usePathname, useRouter } from "@/navigation"
+import { ChangeEvent, ReactNode, useTransition } from "react"
+
+interface LocaleSwitcherSelectProps {
+    children: ReactNode
+    defaultValue: string
+}
+
+export default function LocaleSwitcherSelect({
+    children,
+    defaultValue
+}: LocaleSwitcherSelectProps) {
+    const [isPending, startTransition] = useTransition()
+    const router = useRouter()
+    const pathname = usePathname()
+
+    function onSelectChange(event: ChangeEvent<HTMLSelectElement>) {
+        const nextLocale = event.target.value
+        startTransition(() => router.replace(pathname, { locale: nextLocale }))
+    }
+
+    return (
+        <label className="rounded-md shadow max-sm:mt-2">
+            <p className="sr-only">Change Language</p>
+            <select
+                defaultValue={defaultValue}
+                className="form-select cursor-pointer appearance-none rounded-md border-none bg-transparent text-body-950 focus:ring-accent-600"
+                onChange={onSelectChange}
+                disabled={isPending}
+                aria-label="Language Selector"
+            >
+                {children}
+            </select>
+        </label>
+    )
+}
--- client/src/components/global/locale-switcher.tsx
+++ client/src/components/global/locale-switcher.tsx
@@ -1,35 +1,17 @@
-"use client"
-
-import { useLocale } from "next-intl"
-import { useRouter } from "next/navigation"
-import { type ChangeEvent, useTransition } from "react"
+import LocaleSwitcherSelect from "@/components/global/locale-switcher-selector"
+import { useLocale, useTranslations } from "next-intl"
 
 export default function LocaleSwitcher() {
-    const [isPending, startTransition] = useTransition()
-    const router = useRouter()
-    const localActive = useLocale()
-
-    const onSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
-        const nextLocale = e.target.value
-        startTransition(() => router.replace(`/${nextLocale}`))
-    }
+    const t = useTranslations("LocaleSwitcher")
+    const locale = useLocale()
 
     return (
-        <label className="rounded-md shadow max-sm:mt-2">
-            <p className="sr-only">Change Language</p>
-            <select
-                defaultValue={localActive}
-                className="text-body-950 focus:ring-accent-600 form-select cursor-pointer appearance-none rounded-md border-none bg-transparent"
-                onChange={onSelectChange}
-                disabled={isPending}
-                aria-label="Language Selector"
-            >
-                <option value="" disabled>
-                    Language
-                </option>
-                <option value="en">English</option>
-                <option value="pt-BR">Portuguese</option>
-            </select>
-        </label>
+        <LocaleSwitcherSelect defaultValue={locale}>
+            <option value="" disabled>
+                {t("SelectLanguage")}
+            </option>
+            <option value="en">{t("OptionEnglish")}</option>
+            <option value="pt-BR">{t("OptionPortuguese")}</option>
+        </LocaleSwitcherSelect>
     )
 }
