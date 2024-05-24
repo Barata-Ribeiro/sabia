5e00bfe9bbfae66e6bf2a4ab1fb4159b2f7e962e
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-05-24 10:14:09 -0300

    fix: add missing translations

--- client/src/components/profile/profile-cover-image.tsx
+++ client/src/components/profile/profile-cover-image.tsx
@@ -1,21 +1,29 @@
 import { UserPublicProfileResponse } from "@/interfaces/user"
+import { getTranslations } from "next-intl/server"
 import Image from "next/image"
 
-export default function ProfileCoverImage({
+export default async function ProfileCoverImage({
     profile
 }: {
     profile: UserPublicProfileResponse
 }) {
+    const t = await getTranslations("Profile.Cover")
+
     return (
-        <div id="profile-image" className="relative h-48 w-full">
-            <Image
-                className="pointer-events-none absolute object-cover object-center italic"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                src={profile.cover_image_url}
-                alt="Cover Image"
-                priority
-                fill
-            />
+        <div
+            id="profile-image"
+            className="relative h-48 w-full bg-gradient-to-br from-background-100 to-background-200 dark:from-background-900 dark:to-background-800"
+        >
+            {profile.cover_image_url && (
+                <Image
+                    className="pointer-events-none absolute object-cover object-center italic"
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    src={profile.cover_image_url}
+                    alt={t("AltCover")}
+                    priority
+                    fill
+                />
+            )}
         </div>
     )
 }
