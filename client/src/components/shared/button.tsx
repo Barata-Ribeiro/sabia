69c5f0b56545fd2eb772d3de8eb6ed8f76b34918
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-06-01 15:49:27 -0300

    fix, feat: adjust button component styles, add icon supported button component

--- client/src/components/shared/button.tsx
+++ client/src/components/shared/button.tsx
@@ -7,13 +7,12 @@ export default function Button({
     className,
     ...props
 }: ButtonHTMLAttributes<HTMLButtonElement>) {
-    const sortedClasses = tw`min-h-fit w-full flex-1 select-none
-                                    rounded-lg bg-accent-500 text-body-950 ring-accent-600 transition-all
-                                    hover:bg-accent-600 focus:outline-none active:bg-accent-700 ${
-                                        props.disabled
-                                            ? "opacity-50 cursor-not-allowed"
-                                            : "cursor-pointer" + "hover:opacity-100"
-                                    }`
+    const sortedClasses = tw`min-h-fit w-full flex-1 select-none rounded-lg bg-accent-500
+                                    text-center align-middle text-body-900 ring-accent-600 transition-all
+                                    hover:bg-accent-600 focus:outline-none active:bg-accent-700
+                                    disabled:pointer-events-none disabled:cursor-not-allowed
+                                    disabled:opacity-50 disabled:shadow-none dark:text-body-50`
+
     const mergedClassName = twMerge(sortedClasses, className)
 
     return (
---
+++ client/src/components/shared/icon-button.tsx
@@ -0,0 +1,22 @@
+import tw from "@/utils/tw"
+import type { ButtonHTMLAttributes } from "react"
+import { twMerge } from "tailwind-merge"
+
+export default function IconButton({
+    children,
+    className,
+    ...props
+}: ButtonHTMLAttributes<HTMLButtonElement>) {
+    const sortedClasses = tw`cursor-pointer select-none text-center align-middle
+                                    font-heading text-body-950 transition-all
+                                    focus:outline-none disabled:pointer-events-none
+                                    disabled:cursor-not-allowed disabled:opacity-50 disabled:shadow-none dark:text-body-50`
+
+    const mergedClassName = twMerge(sortedClasses, className)
+
+    return (
+        <button type="button" className={mergedClassName} {...props}>
+            {children}
+        </button>
+    )
+}
