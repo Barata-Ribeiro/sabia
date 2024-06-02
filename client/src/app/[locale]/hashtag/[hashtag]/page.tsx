8d329797d9a901443dd8ffb4f3b19574dde62fb7
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-06-02 11:32:13 -0300

    feat: add accessibility and translations to page and component

--- client/src/app/[locale]/hashtag/[hashtag]/page.tsx
+++ client/src/app/[locale]/hashtag/[hashtag]/page.tsx
@@ -4,6 +4,8 @@ import AsideMenu from "@/components/menu/aside-menu"
 import LinkButton from "@/components/shared/link-button"
 import { PostsHashtagResponse } from "@/interfaces/post"
 import { redirect } from "@/navigation"
+import { Metadata } from "next"
+import { getTranslations } from "next-intl/server"
 import { notFound } from "next/navigation"
 import { HiArrowUturnLeft } from "react-icons/hi2"
 
@@ -12,38 +14,59 @@ interface HashtagPageProps {
     searchParams: { [key: string]: string | string[] | undefined }
 }
 
+export async function generateMetadata({
+    params,
+    searchParams
+}: HashtagPageProps): Promise<Metadata> {
+    const t = await getTranslations("HashtagPage")
+
+    return {
+        title: t("title") + params.hashtag + " | Sabiá",
+        description: t("description") + params.hashtag + " on page " + searchParams.page
+    }
+}
+
 export default async function HashtagPage({ params, searchParams }: HashtagPageProps) {
     if (!params.hashtag) return notFound()
 
+    const t = await getTranslations("HashtagPage")
+
     let page = 0
-    if (!searchParams.page) return redirect(`/hashtag/${params.hashtag}?page=0`)
+    if (!searchParams.page) return redirect("/hashtag/" + params.hashtag + "?page=0")
     else page = parseInt(searchParams.page as string)
 
     const postsState = await getPostsByHashtag({ hashtag: params.hashtag, page })
     const hashtagResponse = postsState.response?.data as PostsHashtagResponse
 
     return (
-        <main role="main" className="flex h-full">
+        <main role="main" className="flex h-full" aria-label={t("AriaLabelMain")}>
             <AsideMenu />
             <section
                 id="hashtag-section"
                 className="max-w-[37.5rem] flex-1 overflow-y-scroll border-x"
+                aria-label={t("AriaLabelHashtag")}
             >
                 <div className="sticky top-0 flex w-full border-b bg-background-50 p-4">
                     <div className="flex items-center gap-6 text-2xl">
                         <LinkButton
                             href="/home"
                             className="cursor-pointer rounded-full p-2 hover:bg-background-100"
+                            aria-label={t("AriaLabelBackButton")}
                         >
                             <HiArrowUturnLeft size={24} />
                         </LinkButton>
                         <h2 className="cursor-default font-heading font-semibold">
-                            Posts with #{params.hashtag}
+                            {t("title")}
+                            {params.hashtag}
                         </h2>
                     </div>
                 </div>
 
-                <PaginatedFeed feedResponse={hashtagResponse} page={page} />
+                {hashtagResponse ? (
+                    <PaginatedFeed feedResponse={hashtagResponse} page={page} />
+                ) : (
+                    <p className="text-center">{t("PageEmptyPosts")}</p>
+                )}
             </section>
         </main>
     )
--- client/src/components/feed/paginated-feed.tsx
+++ client/src/components/feed/paginated-feed.tsx
@@ -1,11 +1,9 @@
 "use client"
 
+import CircularPagination from "@/components/feed/circular-pagination"
 import FeedPost from "@/components/feed/feed-post"
-import IconButton from "@/components/shared/icon-button"
 import { PostsHashtagResponse } from "@/interfaces/post"
-import { usePathname, useRouter } from "next/navigation"
-import { HiMiniArrowLeft, HiMiniArrowRight } from "react-icons/hi2"
-import { twMerge } from "tailwind-merge"
+import { useTranslations } from "next-intl"
 
 interface PaginatedFeedProps {
     feedResponse: PostsHashtagResponse
@@ -13,114 +11,22 @@ interface PaginatedFeedProps {
 }
 
 export default function PaginatedFeed({ feedResponse, page }: PaginatedFeedProps) {
-    const router = useRouter()
-    const pathname = usePathname()
-
-    const goToPage = (pageNumber: number) => {
-        router.push(`${pathname}?page=${pageNumber}`)
-    }
-
-    const handlePrev = () => {
-        if (page > 0) goToPage(page - 1)
-    }
-
-    const handleNext = () => {
-        if (page < feedResponse.total_pages - 1) goToPage(page + 1)
-    }
-
-    const handlePageInput = () => {
-        let pageInput = prompt(
-            `Type the page between 1 and ${feedResponse.total_pages}`
-        )
-        let pageNumber = pageInput ? parseInt(pageInput) : NaN
-        while (
-            isNaN(pageNumber) ||
-            pageNumber < 1 ||
-            pageNumber > feedResponse.total_pages
-        ) {
-            pageInput = prompt(
-                `Type the page between 1 and ${feedResponse.total_pages}`
-            )
-            pageNumber = pageInput ? parseInt(pageInput) : NaN
-        }
-        goToPage(pageNumber - 1)
-    }
+    const t = useTranslations("Feed.Paginated")
 
     return (
         <>
-            <ul className="flex snap-y flex-col divide-y" role="list">
+            <ul
+                className="flex snap-y flex-col divide-y"
+                role="list"
+                aria-label={t("AriaLabelList")}
+            >
                 {feedResponse.posts.map((post) => (
                     <FeedPost post={post} key={post.id} />
                 ))}
             </ul>
 
-            {/*PAGINATION*/}
             {feedResponse.total_pages > 1 && (
-                <div className="my-2 flex flex-col items-center justify-center gap-4 md:flex-row">
-                    <IconButton
-                        type="button"
-                        className="flex items-center gap-2 rounded-full px-6 py-3 text-xs font-bold uppercase text-body-900 hover:bg-background-100 active:bg-background-200"
-                        onClick={handlePrev}
-                        disabled={page === 0}
-                    >
-                        <HiMiniArrowLeft size={16} /> Previous
-                    </IconButton>
-                    <div className="flex items-center gap-2">
-                        {Array.from({
-                            length: Math.min(feedResponse.total_pages, 2)
-                        }).map((_, index) => (
-                            <IconButton
-                                key={index}
-                                className={twMerge(
-                                    "h-10 max-h-[40px] w-10 max-w-[40px] rounded-full text-xs font-bold uppercase text-body-900 hover:bg-background-100 active:bg-background-200",
-                                    page === index && "bg-background-300"
-                                )}
-                                onClick={() => goToPage(index)}
-                            >
-                                {index + 1}
-                            </IconButton>
-                        ))}
-                        {feedResponse.total_pages > 5 && (
-                            <IconButton
-                                className={twMerge(
-                                    "h-10 max-h-[40px] w-10 max-w-[40px] rounded-full text-xs font-bold uppercase text-body-900 hover:bg-background-100 active:bg-background-200",
-                                    page > 1 &&
-                                        page < feedResponse.total_pages - 2 &&
-                                        "bg-background-300"
-                                )}
-                                onClick={handlePageInput}
-                            >
-                                ...
-                            </IconButton>
-                        )}
-                        {Array.from({
-                            length: Math.min(feedResponse.total_pages, 2)
-                        }).map((_, index) => (
-                            <IconButton
-                                key={index}
-                                className={twMerge(
-                                    "h-10 max-h-[40px] w-10 max-w-[40px] rounded-full text-xs font-bold uppercase text-body-900 hover:bg-background-100 active:bg-background-200",
-                                    page === feedResponse.total_pages - 2 + index &&
-                                        "bg-background-300"
-                                )}
-                                onClick={() =>
-                                    goToPage(feedResponse.total_pages - 2 + index)
-                                }
-                            >
-                                {feedResponse.total_pages - 1 + index}
-                            </IconButton>
-                        ))}
-                    </div>
-                    <IconButton
-                        type="button"
-                        className="flex items-center gap-2 rounded-full px-6 py-3 text-xs font-bold uppercase text-body-900 hover:bg-background-100 active:bg-background-200"
-                        onClick={handleNext}
-                        disabled={page === feedResponse.total_pages - 1}
-                    >
-                        Next
-                        <HiMiniArrowRight size={16} />
-                    </IconButton>
-                </div>
+                <CircularPagination totalPages={feedResponse.total_pages} page={page} />
             )}
         </>
     )
