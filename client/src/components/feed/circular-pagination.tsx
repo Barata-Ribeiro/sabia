3137a58eef4de173897a3b7f20c4de6230e216f3
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-06-02 11:31:23 -0300

    refactor: rebuild style and logic of circular pagination for reusability

--- client/src/components/feed/circular-pagination.tsx
+++ client/src/components/feed/circular-pagination.tsx
@@ -1,50 +1,107 @@
-import type { JSX } from "react"
+import IconButton from "@/components/shared/icon-button"
+import { usePathname, useRouter } from "@/navigation"
+import { useTranslations } from "next-intl"
 import { HiMiniArrowLeft, HiMiniArrowRight } from "react-icons/hi2"
+import { twMerge } from "tailwind-merge"
 
-export default function CircularPagination(props: {
-    onClick: () => void
+interface CircularPaginationProps {
+    totalPages: number
     page: number
-    values: number
-    callbackfn: (_: unknown, index: number) => JSX.Element
-    onClick1: () => void
-    callbackfn1: (_: unknown, index: number) => JSX.Element
-    onClick2: () => void
-}) {
+}
+
+export default function CircularPagination({
+    totalPages,
+    page
+}: CircularPaginationProps) {
+    const t = useTranslations("Feed.CircularPagination")
+    const router = useRouter()
+    const pathname = usePathname()
+
+    const goToPage = (pageNumber: number) => {
+        router.push(`${pathname}?page=${pageNumber}`)
+    }
+
+    const handlePrev = () => {
+        if (page > 0) goToPage(page - 1)
+    }
+
+    const handleNext = () => {
+        if (page < totalPages - 1) goToPage(page + 1)
+    }
+
+    const handlePageInput = () => {
+        let pageInput = prompt(t("Prompt") + totalPages)
+        let pageNumber = pageInput ? parseInt(pageInput) : NaN
+        while (isNaN(pageNumber) || pageNumber < 1 || pageNumber > totalPages) {
+            pageInput = prompt(t("Prompt") + totalPages)
+            pageNumber = pageInput ? parseInt(pageInput) : NaN
+        }
+        goToPage(pageNumber - 1)
+    }
+
     return (
-        <div className="flex items-center gap-4">
-            <button
+        <div className="my-2 flex flex-col items-center justify-center gap-4 md:flex-row">
+            <IconButton
                 type="button"
-                className="flex items-center gap-2 rounded-full disabled:opacity-50"
-                onClick={props.onClick}
-                disabled={props.page === 0}
+                className="flex items-center gap-2 rounded-full px-6 py-3 text-xs font-bold uppercase text-body-900 hover:bg-background-100 active:bg-background-200"
+                onClick={handlePrev}
+                disabled={page === 0}
+                aria-disabled={page === 0}
+                aria-label={t("AriaLabelPrevious")}
             >
-                <HiMiniArrowLeft size={16} /> Previous
-            </button>
+                <HiMiniArrowLeft size={16} /> {t("PrevButton")}
+            </IconButton>
             <div className="flex items-center gap-2">
-                {Array.from({ length: Math.min(props.values, 5) }).map(
-                    props.callbackfn
-                )}
-                {props.values > 5 && (
-                    <button
-                        className="cursor-pointer rounded-full px-2 py-1 hover:bg-background-100"
-                        onClick={props.onClick1}
+                {[...Array(Math.min(totalPages, 2))].map((_, index) => (
+                    <IconButton
+                        key={index}
+                        className={twMerge(
+                            "h-10 max-h-[40px] w-10 max-w-[40px] rounded-full text-xs font-bold uppercase text-body-900 hover:bg-background-100 active:bg-background-200",
+                            page === index && "bg-background-300"
+                        )}
+                        onClick={() => goToPage(index)}
+                        aria-label={t("AriaLabelPage") + (index + 1)}
+                    >
+                        {index + 1}
+                    </IconButton>
+                ))}
+                {totalPages > 5 && (
+                    <IconButton
+                        className={twMerge(
+                            "h-10 max-h-[40px] w-10 max-w-[40px] rounded-full text-xs font-bold uppercase text-body-900 hover:bg-background-100 active:bg-background-200",
+                            page > 1 && page < totalPages - 2 && "bg-background-300"
+                        )}
+                        onClick={handlePageInput}
+                        aria-label={t("AriaLabelInput")}
                     >
                         ...
-                    </button>
+                    </IconButton>
                 )}
-                {Array.from({ length: Math.min(props.values, 2) }).map(
-                    props.callbackfn1
-                )}
+                {[...Array(Math.min(totalPages, 2))].map((_, index) => (
+                    <IconButton
+                        key={index}
+                        className={twMerge(
+                            "h-10 max-h-[40px] w-10 max-w-[40px] rounded-full text-xs font-bold uppercase text-body-900 hover:bg-background-100 active:bg-background-200",
+                            page === totalPages - 2 + index && "bg-background-300"
+                        )}
+                        onClick={() => goToPage(totalPages - 2 + index)}
+                        aria-label={t("AriaLabelPage") + (totalPages - 1 + index)}
+                    >
+                        {totalPages - 1 + index}
+                    </IconButton>
+                ))}
             </div>
-            <button
+            <IconButton
                 type="button"
-                className="flex items-center gap-2 rounded-full disabled:opacity-50"
-                onClick={props.onClick2}
-                disabled={props.page === props.values - 1}
+                className="flex items-center gap-2 rounded-full px-6 py-3 text-xs font-bold uppercase text-body-900 hover:bg-background-100 active:bg-background-200"
+                onClick={handleNext}
+                disabled={page === totalPages - 1}
+                aria-disabled={page === totalPages - 1}
+                aria-label={t("AriaLabelNext")}
             >
-                Next
+                {t("NextButton")}
                 <HiMiniArrowRight size={16} />
-            </button>
+            </IconButton>
         </div>
     )
 }
