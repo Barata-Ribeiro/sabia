c138a330e52c0e5fd7fd91f13f926281d9d12308
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-06-17 15:54:42 -0300

    refactor: adjust whitelist authorizations

--- server/src/main/java/com/barataribeiro/sabia/config/SecurityConfig.java
+++ server/src/main/java/com/barataribeiro/sabia/config/SecurityConfig.java
@@ -1,12 +1,10 @@
 package com.barataribeiro.sabia.config;
 
-import com.barataribeiro.sabia.service.security.CustomUserDetailsService;
 import com.barataribeiro.sabia.service.security.SecurityFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,37 +17,42 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import java.util.Arrays;
-import java.util.List;
-
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class SecurityConfig {
+    private static final String[] ROUTES_WHITELIST = {
+            "/h2-console/**",
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/api-docs/**",
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            // -- Sabiá API
+            "/",
+            "/api/v1/auth/**",
+            "/api/v1/posts/public/**",
+            "/api/v1/users/public/**",
+            };
     private final SecurityFilter securityFilter;
-    private final CustomUserDetailsService customUserDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        List<String> public_urls = Arrays.asList("/api/v1/posts/public/**",
-                                                 "/api/v1/users/public/**",
-                                                 "/v3/api-docs/**",
-                                                 "/api-docs/**",
-                                                 "/swagger-resources/**",
-                                                 "/swagger-ui/**");
-
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
-                .authorizeHttpRequests(authorize -> {
-                    authorize.requestMatchers("/h2-console/**").permitAll();
-                    Arrays.asList(HttpMethod.GET, HttpMethod.POST).forEach(httpMethod -> authorize.requestMatchers(httpMethod, "/api/v1/auth/**").permitAll());
-                    for (String url : public_urls) authorize.requestMatchers(HttpMethod.GET, url).permitAll();
-                    Arrays.asList(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE)
-                            .forEach(httpMethod -> authorize.requestMatchers(httpMethod, ("/api/v1/admin/**")).hasRole("ADMIN"));
-                    authorize.anyRequest().authenticated();
-                })
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(ROUTES_WHITELIST).permitAll()
+                        .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
