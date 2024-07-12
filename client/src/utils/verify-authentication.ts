610b73c8765cb62683e546f04b1e2b9cfd2843a0
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-07-12 06:13:30 -0300

    fix: remove wrong call of server action

--- client/src/utils/verify-authentication.ts
+++ client/src/utils/verify-authentication.ts
@@ -1,12 +1,7 @@
-import logout from "@/actions/auth/logout"
 import { cookies } from "next/headers"
 
 export default async function verifyAuthentication(isEnglishLang: boolean) {
     const auth_token = cookies().get("auth_token")?.value
-    if (!auth_token) {
-        await logout()
-        throw new Error(isEnglishLang ? "Unauthorized." : "Não autorizado.")
-    }
-
+    if (!auth_token) throw new Error(isEnglishLang ? "Unauthorized." : "Não autorizado.")
     return auth_token
 }
