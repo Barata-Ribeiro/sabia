c6eb80d8364b6abe03e5ba4e85f4598646b3e8a2
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-07-19 07:55:37 -0300

    feat: add edit account form translations

--- client/src/components/forms/edit-account-form.tsx
+++ client/src/components/forms/edit-account-form.tsx
@@ -9,6 +9,7 @@ import { UserContextResponse } from "@/interfaces/user"
 import { useRouter } from "@/navigation"
 import { NULL_AVATAR } from "@/utils/constants"
 import tw from "@/utils/tw"
+import { useTranslations } from "next-intl"
 import Image from "next/image"
 import { useEffect } from "react"
 import { FaCircleExclamation } from "react-icons/fa6"
@@ -21,6 +22,7 @@ interface EditAccountFormProps {
 }
 
 export default function EditAccountForm({ user, coverBlur, avatarBlur }: Readonly<EditAccountFormProps>) {
+    const t = useTranslations("EditAccountForm")
     const { isPending, formState, formAction, onSubmit } = useForm(putUpdateUserProfile, {
         ok: false,
         clientError: null,
@@ -39,11 +41,11 @@ export default function EditAccountForm({ user, coverBlur, avatarBlur }: Readonl
         <form action={formAction} onSubmit={onSubmit}>
             <fieldset className="mb-6 grid gap-6 rounded-lg border p-4 transition-colors hover:bg-background-100 md:grid-cols-2">
                 <legend className="px-2 font-heading text-sm text-body-900 antialiased dark:text-body-100">
-                    Account Information
+                    {t("AccountInformation.Title")}
                 </legend>
                 <div>
                     <Input
-                        label="Username"
+                        label={t("AccountInformation.InputUsername")}
                         name="username"
                         icon={<HiAtSymbol size={22} />}
                         autoComplete="off"
@@ -52,78 +54,102 @@ export default function EditAccountForm({ user, coverBlur, avatarBlur }: Readonl
                         className="px-3 py-2.5"
                     />
                     <p className="text-body-600">
-                        Your username is <strong>@{user.username}</strong>.
+                        {t("AccountInformation.DescriptionUsername")}
+                        {/* */}
+                        <strong>@{user.username}</strong>.
                     </p>
                 </div>
                 <div>
                     <Input
-                        label="Display name"
+                        label={t("AccountInformation.InputDisplayName")}
                         name="displayName"
                         minLength={3}
                         maxLength={20}
                         className="px-3 py-2.5"
                     />
                     <p className="text-body-600">
-                        Your display name is <strong>{user.displayName}</strong>.
+                        {t("AccountInformation.DescriptionDisplayName")}
+                        {/* */}
+                        <strong>{user.displayName}</strong>.
                     </p>
                 </div>
                 <div>
                     <Input
-                        label="Email"
+                        label={t("AccountInformation.InputEmail")}
                         name="email"
                         type="email"
                         icon={<HiEnvelope size={22} />}
                         className="px-3 py-2.5"
                     />
                     <p className="text-body-600">
-                        Your email address is <strong>{user.email}</strong>.
+                        {t("AccountInformation.DescriptionEmail")}
+                        {/* */}
+                        <strong>{user.email}</strong>.
                     </p>
                 </div>
             </fieldset>
             <fieldset className="mb-6 grid gap-6 rounded-lg border p-4 transition-colors hover:bg-background-100 md:grid-cols-2">
                 <legend className="px-2 font-heading text-sm text-body-900 antialiased dark:text-body-100">
-                    Personal Information
+                    {t("PersonalInformation.Title")}
                 </legend>
                 <div>
                     <div className="grid items-center gap-2 md:grid-cols-2">
-                        <Input label="First name" name="firstName" className="px-3 py-2.5" />
-                        <Input label="Last name" name="lastName" className="px-3 py-2.5" />
+                        <Input
+                            label={t("PersonalInformation.InputFirstName")}
+                            name="firstName"
+                            className="px-3 py-2.5"
+                        />
+                        <Input label={t("PersonalInformation.InputLastName")} name="lastName" className="px-3 py-2.5" />
                     </div>
                     <p className="text-body-600">
-                        Your full name is <strong>{user.fullName}</strong>.
+                        {t("PersonalInformation.DescriptionFullName")}
+                        {/* */}
+                        <strong>{user.fullName}</strong>.
                     </p>
                 </div>
                 <div>
-                    <Input label="Birth date" name="birthDate" type="date" className="px-3 py-2.5" />
+                    <Input
+                        label={t("PersonalInformation.InputBirthDate")}
+                        name="birthDate"
+                        type="date"
+                        className="px-3 py-2.5"
+                    />
                     <p className="text-body-600">
-                        Your birth date is <strong>{user.birthDate}</strong>.
+                        {t("PersonalInformation.DescriptionBirthDate")}
+                        {/* */}
+                        <strong>{user.birthDate}</strong>.
                     </p>
                 </div>
                 <div>
-                    <Input label="Gender" name="gender" icon={<HiIdentification size={22} />} className="px-3 py-2.5" />
+                    <Input
+                        label={t("PersonalInformation.InputGender")}
+                        name="gender"
+                        icon={<HiIdentification size={22} />}
+                        className="px-3 py-2.5"
+                    />
                     <p className="text-body-600">
                         {user.gender
-                            ? `Your gender is <strong>${user.gender}</strong>.`
-                            : `You haven't set your gender yet.`}
+                            ? `${t("PersonalInformation.DescriptionBirthDate")}<strong>${user.gender}</strong>.`
+                            : t("PersonalInformation.NoGender")}
                     </p>
                 </div>
             </fieldset>
             <fieldset className="mb-6 grid gap-6 rounded-lg border p-4 transition-colors hover:bg-background-100 md:grid-cols-2">
                 <legend className="px-2 font-heading text-sm text-body-900 antialiased dark:text-body-100">
-                    Public Information
+                    {t("PublicInformation.Title")}
                 </legend>
                 <div className="flex flex-col gap-2 md:flex-row">
                     <span className="relative aspect-square">
                         <Image
                             src={user.avatarImageUrl ?? NULL_AVATAR}
-                            className="rounded object-cover"
+                            alt={t("PublicInformation.Avatar.Alt")}
+                            className="rounded object-cover italic"
                             placeholder="blur"
                             blurDataURL={avatarBlur}
                             quality={50}
                             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                             priority
                             fill
-                            alt="Avatar Image"
                         />
                     </span>
                     <div className="flex w-max flex-col gap-1 font-heading font-semibold">
@@ -136,10 +162,10 @@ export default function EditAccountForm({ user, coverBlur, avatarBlur }: Readonl
                         />
                         <p className="flex items-center gap-2 text-sm text-primary-700 antialiased">
                             <FaCircleExclamation size={14} />
-                            <span className="w-max">Only HTTPS URLs are allowed.</span>
+                            <span className="w-max">{t("PublicInformation.URLWarning")}</span>
                         </p>
                         <p className="mt-4 max-w-[40ch] text-sm text-body-500">
-                            Please, use a square image for the best results. Follow the rules for the best experience.
+                            {t("PublicInformation.Avatar.Disclaimer")}
                         </p>
                     </div>
                 </div>
@@ -155,7 +181,7 @@ export default function EditAccountForm({ user, coverBlur, avatarBlur }: Readonl
                                 quality={80}
                                 priority
                                 fill
-                                alt="Cover Image"
+                                alt={t("PublicInformation.Cover.Alt")}
                             />
                         </span>
                     )}
@@ -169,11 +195,10 @@ export default function EditAccountForm({ user, coverBlur, avatarBlur }: Readonl
                         />
                         <p className="flex w-fit items-center gap-2 text-sm text-primary-700 antialiased">
                             <FaCircleExclamation size={14} />
-                            <span className="w-max">Only HTTPS URLs are allowed.</span>
+                            <span className="w-max">{t("PublicInformation.URLWarning")}</span>
                         </p>
                         <p className="mt-4 max-w-[40ch] text-sm text-body-500">
-                            Please, use a rectangle image for the best results. Follow the rules for the best
-                            experience.
+                            {t("PublicInformation.Cover.Disclaimer")}
                         </p>
                     </div>
                 </div>
@@ -184,18 +209,18 @@ export default function EditAccountForm({ user, coverBlur, avatarBlur }: Readonl
                             name="newBio"
                             rows={3}
                             maxLength={280}
-                            aria-label="Biography"
+                            aria-label={t("PublicInformation.InputBio")}
                             className={textAreaStyle}
                             placeholder=" "
                         ></textarea>
                         <label htmlFor="new-bio" className={labelTextAreaStyle}>
-                            Biography
+                            {t("PublicInformation.InputBio")}
                         </label>
                     </div>
                     <p className="text-body-600">
                         {user.biography
-                            ? `Your biography is <strong>${user.biography}</strong>.`
-                            : `You haven't set your biography yet.`}
+                            ? `${t("PublicInformation.DescriptionBio")}<strong>${user.biography}</strong>.`
+                            : t("PublicInformation.NoBio")}
                     </p>
                 </div>
                 <div className="flex flex-col gap-2">
@@ -209,46 +234,53 @@ export default function EditAccountForm({ user, coverBlur, avatarBlur }: Readonl
                         />
                         <p className="text-body-600">
                             {user.website
-                                ? `Your website is <strong>${user.website}</strong>.`
-                                : `You haven't set your website yet.`}
+                                ? `${t("PublicInformation.DescriptionWebsite")}<strong>${user.website}</strong>.`
+                                : t("PublicInformation.NoWebsite")}
                         </p>
                     </div>
                     <div>
                         <div className="flex gap-2">
-                            <Input label="State" name="locState" className="px-3 py-2.5" />
-                            <Input label="Country" name="locCountry" className="px-3 py-2.5" />
+                            <Input
+                                type="text"
+                                label={t("PublicInformation.Location.InputState")}
+                                name="locState"
+                                className="px-3 py-2.5"
+                            />
+                            <Input
+                                type="text"
+                                label={t("PublicInformation.Location.InputCountry")}
+                                name="locCountry"
+                                className="px-3 py-2.5"
+                            />
                         </div>
                         <p className="text-body-600">
                             {user.location
-                                ? `Your location is <strong>${user.location}</strong>.`
-                                : `You haven't set your location yet.`}
+                                ? `${t("PublicInformation.Location.Description")}<strong>${user.location}</strong>.`
+                                : t("PublicInformation.Location.NoLocation")}
                         </p>
                     </div>
                 </div>
             </fieldset>
             <fieldset className="mb-6 grid gap-6 rounded-lg border p-4 transition-colors hover:bg-background-100 md:grid-cols-2">
                 <legend className="px-2 font-heading text-sm text-body-900 antialiased dark:text-body-100">
-                    Security Information
+                    {t("SecurityInformation.Title")}
                 </legend>
                 <div>
                     <Input
-                        label="New Password"
-                        name="newPassword"
                         type="password"
-                        autoComplete="new-password"
-                        icon={<HiLockClosed size={22} />}
+                        label={t("SecurityInformation.InputNewPassword")}
+                        name="newPassword"
                         className="px-3 py-2.5"
+                        icon={<HiLockClosed size={22} />}
+                        autoComplete="new-password"
                     />
                     <p className="flex w-fit items-center gap-2 self-center text-sm text-primary-700 antialiased">
                         <FaCircleExclamation size={14} />
-                        <span className="w-[55ch]">
-                            Use at least 8 characters, one uppercase, one lowercase, a special character, and one
-                            number.
-                        </span>
+                        <span className="w-[55ch]">{t("SecurityInformation.PasswordRequirements")}</span>
                     </p>
                 </div>
                 <Input
-                    label="Confirm Password"
+                    label={t("SecurityInformation.InputConfirmPassword")}
                     name="confirmPassword"
                     type="password"
                     autoComplete="off"
@@ -258,11 +290,9 @@ export default function EditAccountForm({ user, coverBlur, avatarBlur }: Readonl
             </fieldset>
             <div className="flex flex-col gap-6 self-center">
                 <div className="flex max-w-[20rem] flex-col gap-4 self-center">
-                    <p className="text-pretty text-center text-xl leading-6">
-                        To edit your account, you need to provide your current password.
-                    </p>
+                    <p className="text-pretty text-center text-xl leading-6">{t("ConfirmChanges.Disclaimer")}</p>
                     <Input
-                        label="Current Password"
+                        label={t("ConfirmChanges.InputPassword")}
                         name="currentPassword"
                         type="password"
                         autoComplete="current-password"
@@ -284,20 +314,20 @@ export default function EditAccountForm({ user, coverBlur, avatarBlur }: Readonl
                         htmlFor="terms-of-use"
                         className="inline-flex gap-1 text-body-600 dark:text-body-50 lg:text-body-950"
                     >
-                        I still agree with the
+                        {t("ConfirmChanges.Terms.Part-1")}
                         <LinkButton
                             href={"/terms-of-use"}
                             className="font-heading text-body-600 transition-colors duration-300 hover:text-accent-500 hover:underline dark:text-body-400 dark:hover:text-accent-200"
                             target="_blank"
                             rel="noopener noreferrer"
                         >
-                            Terms of use
+                            {t("ConfirmChanges.Terms.Link")}
                         </LinkButton>
-                        while editing my account.
+                        {t("ConfirmChanges.Terms.Part-2")}
                     </label>
                 </div>
-                <Button type="submit" className="py-2" disabled={isPending} aria-disabled={isPending}>
-                    {isPending ? "Processing..." : "Edit Account"}
+                <Button type="submit" className="py-2 font-medium" disabled={isPending} aria-disabled={isPending}>
+                    {isPending ? t("ConfirmChanges.ButtonLoading") : t("ConfirmChanges.ButtonSubmit")}
                 </Button>
             </div>
         </form>
